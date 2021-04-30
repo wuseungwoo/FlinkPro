f6ce742353985f27cb7500c3431655456a51e986
wuseungwoo <wsy19931119@gmail.com>
2021-04-30 14:37:49 +0800

    更新

--- src/main/scala/com/seungwoo/datastreamconnector/sink/Flink_ESSink.scala
+++ src/main/scala/com/seungwoo/datastreamconnector/sink/Flink_ESSink.scala
@@ -1,11 +1,26 @@
 package com.seungwoo.datastreamconnector.sink
 
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
 object Flink_ESSink {
   def main(args: Array[String]): Unit = {
     //Elasticsearch 在项目中主要用于实时数仓,用户画像
     //因此掌握Elasticsearch的数据写入是非常有必要的
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-    //test my test huhu jiji
+    val topic:String = "flink_kafka"
+    val properties = new Properties()
+
+    properties.setProperty("bootstrap.servers", "localhost:9092")
+
+    val kafkaStream: DataStream[String] = env.addSource(new FlinkKafkaConsumer[String](
+      topic, new SimpleStringSchema(), properties
+    ))
+    kafkaStream.addSink(new ElasticsearchSink[String]())
   }
-
 }
